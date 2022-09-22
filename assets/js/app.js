76fe87a2e50d6252cc103ad2dbaf25c02ba75ad7
riGGed <local.riGGed@email.com>
2022-09-22 08:55:17 +0100

    First javascript calculation

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+// console.log("Hello world")
+// const n = prompt("Enter your name")
+// alert ("Hi " + n + " Welcome to javascript")
+
+const p1 = 100;
+const r1 = 5.0;
+const t1 = 5;
+let i;
+i = p1 * r1 * t1 / 100;
+alert("Calculated simple interest with the given parameters = " + i);
+
+
+
+
+alert("Now it's time for you to calculate with any values ");
+const p = prompt("Enter the principal: ");
+const r = prompt("Enter the interest rate: ");
+const t = prompt("Enter the number of years: ");
+let si = p * r * t / 100;
+alert("Calculated simple interest with the given parameters = " + si);
