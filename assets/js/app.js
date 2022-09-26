0cf2d9805870a7131c0160adcfe658f155762937
riGGed <local.riGGed@email.com>
2022-09-26 15:01:01 +0100

    Added solution for quadratic equation involving complex roots

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,13 +20,37 @@
 
 //To calculate compound interest
 
-let p, t, r, n, i, x, y;
-p = 5000;
-t = 3;
-r = 4;
-n = 2;
-r = r / 100;
-i = Math.pow(p * (1 - r / n), (n*t));
+// let p, t, r, n, a;
+// p = 5000;
+// t = 3;
+// r = 4;
+// n = 1;
+// r = r / 100;
+// a = p * Math.pow(1 - (r / n)), (n * t);
 
-console.log("Interest rate after 3 years = ", i);
-console.log("Interest rate plus principal after 3 years = ", i + p);
+// console.log("Interest rate after 3 years = ", a);
+// console.log("Interest rate plus principal after 3 years = ", a + p);
+
+const a = parseFloat( prompt("Enter value for a") );
+const b = parseFloat( prompt("Enter value for b") );
+const c = parseFloat( prompt("Enter value for c") );
+const d = Math.pow(b, 2) - (4 * a * c)
+let x1, x2;
+
+if( d > 1){
+x1 = (-b + Math.sqrt(d)) / (2 * a);
+x2 = (-b - Math.sqrt(d)) / (2 * a);
+}
+
+else{
+    console.log("Equation leads to complex roots which gives");
+    const x11 = (-b / (2 * a));
+    const x12 = ( Math.sqrt(Math.abs(d))) / (2 * a);
+    // const x21 = (-b / (2 * a));
+    // const x22 = ( Math.sqrt(Math.abs(d))) / (2 * a);
+
+    x1 = x11 + " + i" + x12;
+    x2 = x11 + " - i" + x12;
+}
+
+console.log("Solution to the quadratic equation: X1 = ", x1, ",            X2 = ", x2);
