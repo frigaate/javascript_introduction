5621e1b83100a4629efe9bca7eae4559145498f9
riGGed <local.riGGed@email.com>
2022-10-07 14:52:24 +0100

    Added basic loops in javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,3 +79,56 @@ switch(x){
         console.log("Your BMI is within the healthy range");
 
 }
+
+
+const arr = [4, 9, 10, 20]
+const arr1 = [3, 0, 1, 2]
+
+// arr.push(25, 23, 90, "hello", true, 2.264, ["Nice", true, 3.142])
+// arr.unshift()
+// arr.pop()
+// arr.shift()
+
+console.log(arr)
+
+const person = {
+    name: "Akpos", age: 46, isMarried: false
+}
+
+// To access any property of an array we use
+// dot notation 
+person.name
+// or reassign it with 
+person.name = "Olusegun"
+// We can add property and value to an object by using
+person.yearOfBirth = 1976
+//An object value can also be retrieved or assigned with bracket notation as
+person["obj"]
+//To access a value, the property inside the  square beacket, must be a string
+//To add property and value can also be done with the square bracket by
+person["is cute"] = false
+//When a peoperty is creater with space between, it must always be referenced with square bracket method and quotation mark, because of the space, as using the dot method will not recognise the space
+
+console.log(person)
+
+// const scores = [22, 54, 76, 92, 43, 33];
+// console.log(scores.length);
+
+// let i = 0;
+// while(i < scores.length){
+//     console.log(scores[i]+2);
+//     i++;
+// }
+
+// do{
+//     consolee.log(scores[i]);
+//     i++;
+// }
+// while(i < scores.length);
+
+const score = [1, 2, 3, 4, 5, 6];
+
+for(i = 0; i < score.length; i++)
+{
+    console.log(score[i] + 2);
+}
